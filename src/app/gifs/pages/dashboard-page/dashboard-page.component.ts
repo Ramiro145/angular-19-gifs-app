7328d0d31433baba74e81c5c6639ab706bd9eaf4
Ramiro <ramirogc145@gmail.com>
2025-10-14 18:27:53 -0600

    side menu components

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GifsSideMenuHeaderComponent } from "../../components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component";
+import { GifsSideMenuOptionsComponent } from "../../components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component";
+import { GifsSideMenuComponent } from "../../components/gifs-side-menu/gifs-side-menu.component";
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [RouterOutlet, GifsSideMenuComponent],
+  templateUrl: './dashboard-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class DashboardPageComponent { }
