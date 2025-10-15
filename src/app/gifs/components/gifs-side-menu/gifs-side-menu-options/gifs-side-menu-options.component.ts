83fcc5366c8d295fc0fa9f38c9fd112ddb551a78
Ramiro <ramirogc145@gmail.com>
2025-10-14 18:46:53 -0600

    RouterLinks lesson

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,9 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption{
+  label:string;
+  subLabel:string;
+  route:string;
+  icon:string
+}
+
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+
+  menuOptions:MenuOption[] = [
+    {
+      icon:'fa-solid fa-chart-line',
+      label:'Trending',
+      subLabel:'Gifs populares',
+      route:'/dashboard/trending'
+    },
+    {
+      icon:'fa-solid fa-magnifying-glass',
+      label:'Buscador',
+      subLabel:'Buscar gifs',
+      route:'/dashboard/search'
+    }
+  ]
+
+
+ }
