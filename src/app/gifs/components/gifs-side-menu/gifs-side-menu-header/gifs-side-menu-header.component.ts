1870ac9d937192bf27f10ed59f3cd0a3b91362a9
Ramiro <ramirogc145@gmail.com>
2025-10-15 15:43:37 -0600

    feature/update environment files

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
@@ -6,4 +7,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './gifs-side-menu-header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GifsSideMenuHeaderComponent { }
+export class GifsSideMenuHeaderComponent {
+
+    envs = environment;
+
+
+ }
